1fa676620126173730dc0f60aca774e1a6031c1c
Slightly <oaskew@gmail.com>
2017-02-02 15:08:41 +1100

    chore: body text amend

--- src/components/Test.js
+++ src/components/Test.js
@@ -9,7 +9,7 @@ class Test extends Component {
     return(
       <div styleName="meh" className="moo">
         <h1><Link to="/">Back home you say. Very well.</Link></h1>
-        <p styleName="element">My ghastly styles are locally scoped and load into the document head via "styleName" attribute from an anonymously named css module created from test.scss, which also accesses and parses @import styles from ~/src/style/var.scss. Any html or style edits will hot reload. The production build is configured to load into an external file via extract-text-webpack-plugin but I have not yet considered spill from global styles.</p>
+        <p styleName="element">My ghastly styles are parsed through scss-loader and postcss-loader before being locally scoped via babel-plugin-react-css-modules and loaded into the document head. The JSX uses the "styleName" attribute and imports test.scss. In order to allow sharing of default styles whilst also using the higher-performance anoymous naming from babel-plugin-react-css-modules, test.scss accesses and parses @import styles into local css modules from ~/src/style/var.scss. Any html or style edits down the chain will all hot reload. The production build is configured to load into an external file via extract-text-webpack-plugin but I have not yet considered spill from global styles.</p>
         <p className="sans-serif">I load global styles from ~/src/global.scss because I use "className" attribute. extract-text-webpack-plugin loads them straight into styles.css.  Subject to specificity, global styles will usually be overwritten by the css modules since they're in the head. Use global styles sparingly. Global styles don't hot reload. </p>
       </div>
     );
